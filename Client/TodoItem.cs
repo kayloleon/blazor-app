dc4f617886eb6efda03922414663560ea383dfd5
Kaylo Leon <kleon5532@eagle.fgcu.edu>
2021-09-29 21:42:58 -0400

    updated blazor app, added todo list, changed text and icons

---
+++ Client/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// creates class which is later used in Todo.razor when making to do list
+// this class represents each item in the to do list
+public class TodoItem
+{
+    // item Title given by user in Todo.razor
+    public string Title { get; set; }
+
+    // Tells if item is checked, given by user in Todo.razor
+    public bool IsDone { get; set; }
+
+}
